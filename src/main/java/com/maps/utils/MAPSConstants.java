78bbc256f27ff9b94be04c1ea4c7c9792efd20e3
Nageswar <enageswararao@gmail.com>
2019-11-26 00:38:41 +0530

    first commit

---
+++ src/main/java/com/maps/utils/MAPSConstants.java
@@ -0,0 +1,48 @@
+package com.maps.utils;
+
+import java.util.Hashtable;
+
+public class MAPSConstants {
+	public static final boolean GRID_RUN=true;
+	
+	//paths
+	public static final String CHROME_DRIVER_EXE="C:\\Users\\ammulu123\\workspace\\DemoProject2\\chromedriver\\chromedriver.exe";
+	public static final String GECOKO_DRIVER_EXE="C:\\Users\\ammulu123\\workspace\\DemoProject2\\chromedriver\\chromedriver.exe";
+
+	
+	// locators
+	public static final String SEARCHBOX = "searchboxinput";
+	public static final String SEARCHBUTTON = "searchbox-searchbutton";
+	public static final String HEADERTITTLE = "//h1[text()='Dublin']";
+	public static final String DIRECTIONICON = "//div[text()='Directions']";	
+	public static final String DESTINATIONBOX="//*[@id='sb_ifc52']/input";
+	
+ 
+
+	// URLs-QA
+	public static final String QA_HOMEPAGE_URL = "https://www.QA.google.com/maps";
+
+	// URLs-uat
+	public static final String UAT_HOMEPAGE_URL = "https://www.google.com/maps";
+
+	
+	public static final String ENV="PROD"; //PROD, UAT,SAT 
+			
+
+	
+}
+
+	
+
+
+	
+
+
+
+
+	
+
+
+
+	
+ 
